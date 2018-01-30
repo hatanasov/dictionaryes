9a6314b7afcbfcc2b9662f123e239d761bf592b8
hatanasov <h.atanasov87@gmail.com>
2018-01-30 13:13:02 +0200

    Initial commit

---
+++ oddoccur.py
@@ -0,0 +1,14 @@
+words = input().split()
+
+word_occur = {}
+
+for word in words:
+    word = word.lower()
+    if word not in word_occur:
+        word_occur[word] = 1
+    else:
+        word_occur[word] += 1
+
+result = [word for word in word_occur if word_occur[word] % 2 == 1]
+
+print(*result)
